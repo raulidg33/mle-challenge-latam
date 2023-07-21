27e03ffa9efaab7c6607f4d642a89c428b9ccefd
raulidg33 <rdastres@alumnos.uai.cl>
2023-07-21 01:25:11 -0400

    add column validation to preprocess method

--- challenge/model.py
+++ challenge/model.py
@@ -31,6 +31,45 @@ class DelayModel:
             "OPERA_Sky Airline",
             "OPERA_Copa Air"
         ]
+        self.__all_columns = [
+            'OPERA_Aerolineas Argentinas',
+            'OPERA_Aeromexico',
+            'OPERA_Air Canada',
+            'OPERA_Air France',
+            'OPERA_Alitalia',
+            'OPERA_American Airlines',
+            'OPERA_Austral',
+            'OPERA_Avianca',
+            'OPERA_British Airways',
+            'OPERA_Copa Air',
+            'OPERA_Delta Air',
+            'OPERA_Gol Trans',
+            'OPERA_Grupo LATAM',
+            'OPERA_Iberia',
+            'OPERA_JetSmart SPA',
+            'OPERA_K.L.M.',
+            'OPERA_Lacsa',
+            'OPERA_Latin American Wings',
+            'OPERA_Oceanair Linhas Aereas',
+            'OPERA_Plus Ultra Lineas Aereas',
+            'OPERA_Qantas Airways',
+            'OPERA_Sky Airline',
+            'OPERA_United Airlines',
+            'TIPOVUELO_I',
+            'TIPOVUELO_N',
+            'MES_1',
+            'MES_2',
+            'MES_3',
+            'MES_4',
+            'MES_5',
+            'MES_6',
+            'MES_7',
+            'MES_8',
+            'MES_9',
+            'MES_10',
+            'MES_11',
+            'MES_12'
+        ]
         curdir = os.path.dirname(os.path.realpath(__file__))
         self.__savpath = os.path.join(curdir, 'xgbc.sav')
         self.__threshold_in_minutes = 15
@@ -76,11 +115,15 @@ class DelayModel:
             pd.DataFrame: features.
         """
         features = pd.concat([
+            pd.DataFrame(columns=self.__selected_features),
             pd.get_dummies(data['OPERA'], prefix = 'OPERA'),
             pd.get_dummies(data['TIPOVUELO'], prefix = 'TIPOVUELO'),
             pd.get_dummies(data['MES'], prefix = 'MES')],
             axis = 1
-        )[self.__selected_features]
+        ).fillna(False)
+        if not all(column in self.__all_columns for column in features.columns):
+            raise ValueError('Unknown column on data.')
+        features = features[self.__selected_features]
         if target_column:
             data['min_diff'] = data.apply(self.__get_min_diff, axis = 1)
             data['delay'] = np.where(data['min_diff'] > self.__threshold_in_minutes, 1, 0)
